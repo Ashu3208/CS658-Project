d69adbf619d4b42a2a6c2970cee60235dbec118e
Ashu3208 <ashutoshagrawal822@gmail.com>
2024-11-08 03:51:39 +0530

    Frotend requesting to flask server

--- templates/script.js
+++ templates/script.js
@@ -6,7 +6,7 @@ document.getElementById('urlForm').addEventListener('submit', async function(eve
 
     // Send the data as JSON via a POST request
     try {
-        const response = await fetch('http://localhost:5000/predict', {
+        const response = await fetch('https://cs658-5den.onrender.com/predict', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
